f27a0ac11bcf3ae991a596df53832a75fd9dae3c
Sergey5588 <sergey271109@gmail.com>
2026-04-16 19:51:38 +0300

    VSync and fullscreen

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@
 static const Uint32 FONT_ID = 0;
 static const Clay_Color COLOR_ORANGE = (Clay_Color){225, 138, 50, 255};
 static const Clay_Color COLOR_BLUE   = (Clay_Color){111, 173, 162, 255};
-static const Clay_Color COLOR_LIGHT  = (Clay_Color){224, 215, 210, 255};
+static const Clay_Color COLOR_WHITE  = (Clay_Color){255,255,255,255};
 
 
 static const unsigned char s_font_roboto[] = {  
@@ -24,6 +24,18 @@ static const unsigned char s_font_roboto[] = {
 #define CLAY_SDL3_IMPLEMENTATION
 #include "app.h"
 #include "components.h"
+
+
+static inline Clay_String Clay__FormatString(char *buf, int size, const char *fmt, ...) {  
+    va_list args;  
+    va_start(args, fmt);  
+    int len = vsnprintf(buf, size, fmt, args);  
+    va_end(args);  
+    return (Clay_String){ .isStaticallyAllocated = false, .length = len, .chars = buf };  
+}  
+  
+#define CLAY_FSTRING(size, fmt, ...) \
+    Clay__FormatString((char[size]){0}, size, fmt, ##__VA_ARGS__)
 // ----------------------------------------------------------------------------
 // STB image loading 
 // ----------------------------------------------------------------------------
@@ -58,20 +70,21 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     *appstate = state;
 	state->cam.zoom = 1.0f;
     // Create window and renderer
-    if (!SDL_CreateWindowAndRenderer("Factory game", 640, 480, 0,
+    if (!SDL_CreateWindowAndRenderer("Factory game", 640, 480, SDL_WINDOW_FULLSCREEN,
                                      &state->window, &state->renderer)) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
     SDL_SetWindowResizable(state->window, true);
-
+	if (!SDL_SetRenderVSync(state->renderer, 1)) {
+		SDL_Log("Failed to enable VSync: %s", SDL_GetError());
+	}
     // Load sample image
     state->sampleImage = STB_LoadTexture(state->renderer, "resources/cat.png", NULL, NULL);
     if (!state->sampleImage) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to load image");
         return SDL_APP_FAILURE;
     }
-
 	state->ecs = ecs_init();  
 	if(!state->ecs) {
 		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "ecs_init() returned NULL");
@@ -119,17 +132,31 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
     switch (event->type) {
         case SDL_EVENT_QUIT:
             return SDL_APP_SUCCESS;
-		case SDL_EVENT_WINDOW_RESIZED:
-			SDL_GetWindowSize(state->window, &state->window_w, &state->window_h);
+		case SDL_EVENT_WINDOW_EXPOSED:
+			SDL_AppIterate(state);
+			break;
+		case SDL_EVENT_WINDOW_PIXEL_SIZE_CHANGED:
+			state->window_w = event->window.data1;
+			state->window_h = event->window.data2;
 			break;
 		case SDL_EVENT_MOUSE_WHEEL:
-			state->cam.zoom-=event->wheel.y/10.0f;
+			if(state->cam.zoom > 0.09)
+				state->cam.zoom-=event->wheel.y/10.0f; // 0.1 or -0.1
+			if(state->cam.zoom <= 0.1) {
+				state->cam.zoom = 0.1;
+			}
 			break;
 		case SDL_EVENT_MOUSE_MOTION:
 			if (event->motion.state & SDL_BUTTON_LMASK) {
 				state->cam.x+= event->motion.xrel;
 				state->cam.y+= event->motion.yrel;
 			}
+			break;
+		case SDL_EVENT_KEY_DOWN:  
+			if (event->key.key == SDLK_F11) {  
+				bool is_fullscreen = (SDL_GetWindowFlags(state->window) & SDL_WINDOW_FULLSCREEN) != 0;  
+				SDL_SetWindowFullscreen(state->window, !is_fullscreen);  
+			}  
 			break;
         default:
             break;
@@ -147,15 +174,15 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 			.sizing  = { CLAY_SIZING_GROW(0), CLAY_SIZING_FIT(0) },  
 			.padding = CLAY_PADDING_ALL(8),  
 		},  
-		.backgroundColor = (Clay_Color){ 30, 30, 30, 200 },  
+		.backgroundColor = {0},  
 	})
 	 {  
         CLAY_TEXT(  
-            CLAY_STRING("Factory Game"),  
+            CLAY_FSTRING(64,"resolution: %d %d", state->window_w, state->window_h),  
             CLAY_TEXT_CONFIG({  
                 .fontId    = FONT_ID,  
-                .fontSize  = 20,  
-                .textColor = COLOR_LIGHT,  
+                .fontSize  = 24,  
+                .textColor = COLOR_WHITE,  
             })  
         );  
     }  
@@ -181,6 +208,7 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result) {
         if (state->renderer) SDL_DestroyRenderer(state->renderer);
         if (state->window) SDL_DestroyWindow(state->window);
 		if (state->ecs) ecs_fini(state->ecs);
+		if (state->clayMemory) SDL_free(state->clayMemory);
         SDL_free(state);
     }
 }
